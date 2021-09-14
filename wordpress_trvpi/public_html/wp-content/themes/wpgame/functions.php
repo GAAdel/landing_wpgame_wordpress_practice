86e41b2992de5404eb9047e45443bb2f73b2c273
GAAdel <32236445+GAAdel@users.noreply.github.com>
2021-09-14 13:52:01 +0300

    created index.php & finctions.php

---
+++ wordpress_trvpi/public_html/wp-content/themes/wpgame/functions.php
@@ -0,0 +1,12 @@
+<?php
+
+// правильный способ подключить стили и скрипты
+add_action('wp_enqueue_scripts', 'wpgame_scripts');
+// add_action('wp_print_styles', 'theme_name_scripts'); // можно использовать этот хук он более поздний
+function wpgame_scripts()
+{
+  wp_enqueue_style('style-name', get_stylesheet_uri());
+  wp_enqueue_style('style-min', get_template_directory_uri() . '/css/style.min.css');
+
+  wp_enqueue_script('script', get_template_directory_uri() . '/js/script.js', array(), '1.0.0', true);
+}
